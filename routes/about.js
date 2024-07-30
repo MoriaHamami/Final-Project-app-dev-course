9a3ed27e4978598cb5c975474f2b042c013ba96d
Moria Hamami <moriahamami@gmail.com>
2024-07-30 13:43:12 +0300

    Limit editing to manager only

--- routes/about.js
+++ routes/about.js
@@ -5,29 +5,31 @@ const loginController = require("../controllers/login");
 
 router.route('/')
   .get(aboutController.getAboutPage)
-  .post(aboutController.createNew);
-  // .post(loginController.isManagerLoggedIn,aboutController.createNew);
+  // .post(aboutController.createNew);
+  .post(loginController.isManagerLoggedIn,aboutController.createNew)
   router.route('/edit-shops')
-  .get(aboutController.getEditAboutPage)
-  .put(aboutController.updateShops)
+  // .get(aboutController.getEditAboutPage)
+  .get(loginController.isManagerLoggedIn,aboutController.getEditAboutPage)
+  // .put(aboutController.updateShops)
+  .put(loginController.isManagerLoggedIn,aboutController.updateShops)
 
 router.route('/edit')
-  .get((req, res) => {
+  // .get((req, res) => {
+  //   res.render('edit-news.ejs', { article: null })
+  // })
+  // .post(aboutController.createNew) // הוספנו כאן את הטיפול בבקשות POST
+  .get(loginController.isManagerLoggedIn,(req, res) => {
     res.render('edit-news.ejs', { article: null });
   })
-  .post(aboutController.createNew); // הוספנו כאן את הטיפול בבקשות POST
-  // .get(loginController.isManagerLoggedIn,(req, res) => {
-  //   res.render('edit-news.ejs', { article: null });
-  // })
-  // .post(loginController.isManagerLoggedIn,aboutController.createNew); // הוספנו כאן את הטיפול בבקשות POST
+  .post(loginController.isManagerLoggedIn,aboutController.createNew); // הוספנו כאן את הטיפול בבקשות POST
 
 router.route('/edit/:id')
-  .get(aboutController.getNew)
-  .put(aboutController.updateNew)
-  .delete(aboutController.deleteNew);
-  // .get(loginController.isManagerLoggedIn,aboutController.getNew)
-  // .put(loginController.isManagerLoggedIn,aboutController.updateNew)
-  // .delete(loginController.isManagerLoggedIn,aboutController.deleteNew);
+  // .get(aboutController.getNew)
+  // .put(aboutController.updateNew)
+  // .delete(aboutController.deleteNew);
+  .get(loginController.isManagerLoggedIn,aboutController.getNew)
+  .put(loginController.isManagerLoggedIn,aboutController.updateNew)
+  .delete(loginController.isManagerLoggedIn,aboutController.deleteNew);
 
 router.route('/search')
   .get(aboutController.searchNews);
